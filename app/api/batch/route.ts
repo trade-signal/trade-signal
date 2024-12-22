1cd288668eff0070c6d8a38b7b808acb339dca96
heora <yueluo.yang@gmail.com>
2024-12-22 15:41:05 +0800

    chore: add batch api

---
+++ app/api/batch/route.ts
@@ -0,0 +1,42 @@
+import prisma from "@/prisma/db";
+
+export const GET = async () => {
+  const latestBatches = await prisma.batchUpdate.groupBy({
+    by: ["type", "source"],
+    _max: {
+      batchTime: true
+    },
+    where: {
+      status: "completed"
+    }
+  });
+
+  const result = await Promise.all(
+    latestBatches.map(async batch => {
+      return await prisma.batchUpdate.findFirst({
+        select: {
+          id: true,
+          type: true,
+          source: true,
+          batchTime: true,
+          count: true,
+          status: true,
+          error: true
+        },
+        where: {
+          type: batch.type,
+          source: batch.source,
+          batchTime: batch._max.batchTime as Date
+        },
+        orderBy: {
+          batchTime: "desc"
+        }
+      });
+    })
+  );
+
+  return Response.json({
+    success: true,
+    data: result
+  });
+};
