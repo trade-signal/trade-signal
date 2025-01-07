e9a54c92e621066728e066010b54d8a7991d4813
heora <yueluo.yang@gmail.com>
2025-01-08 07:21:26 +0800

    feat: add footer intro

---
+++ app/components/common/Footer/index.tsx
@@ -0,0 +1,117 @@
+import {
+  IconBrandInstagram,
+  IconBrandTwitter,
+  IconBrandYoutube
+} from "@tabler/icons-react";
+import {
+  ActionIcon,
+  Container,
+  Group,
+  Image,
+  rem,
+  Text,
+  Title
+} from "@mantine/core";
+import classes from "./index.module.css";
+
+const data = [
+  {
+    title: "About",
+    links: [
+      { label: "Features", link: "#" },
+      { label: "Pricing", link: "#" },
+      { label: "Support", link: "#" },
+      { label: "Forums", link: "#" }
+    ]
+  },
+  {
+    title: "Project",
+    links: [
+      { label: "Contribute", link: "#" },
+      { label: "Media assets", link: "#" },
+      { label: "Changelog", link: "#" },
+      { label: "Releases", link: "#" }
+    ]
+  },
+  {
+    title: "Community",
+    links: [
+      { label: "Join Discord", link: "#" },
+      { label: "Follow on Twitter", link: "#" },
+      { label: "Email newsletter", link: "#" },
+      { label: "GitHub discussions", link: "#" }
+    ]
+  }
+];
+
+const FooterLinks = () => {
+  const groups = data.map(group => {
+    const links = group.links.map((link, index) => (
+      <Text<"a">
+        key={index}
+        className={classes.link}
+        component="a"
+        href={link.link}
+        onClick={event => event.preventDefault()}
+      >
+        {link.label}
+      </Text>
+    ));
+
+    return (
+      <div className={classes.wrapper} key={group.title}>
+        <Text className={classes.title}>{group.title}</Text>
+        {links}
+      </div>
+    );
+  });
+
+  return (
+    <footer className={classes.footer}>
+      <Container className={classes.inner}>
+        <div className={classes.logo}>
+          <Group gap={5}>
+            <Image
+              style={{ width: rem(26), height: rem(26) }}
+              src="/icon.svg"
+              alt="TradeSignal logo"
+            />
+            <Title order={3} visibleFrom="xl">
+              <Text fw="bolder" size="xs" inherit>
+                TradeSignal
+              </Text>
+            </Title>
+          </Group>
+          <Text size="xs" c="dimmed" className={classes.description}>
+            富贵险中求，也在险中丢；求时十之一，丢时十之九。
+          </Text>
+        </div>
+        <div className={classes.groups}>{groups}</div>
+      </Container>
+      <Container className={classes.afterFooter}>
+        <Text c="dimmed" size="sm">
+          © 2025 TradeSignal. All rights reserved.
+        </Text>
+
+        <Group
+          gap={0}
+          className={classes.social}
+          justify="flex-end"
+          wrap="nowrap"
+        >
+          <ActionIcon size="lg" color="gray" variant="subtle">
+            <IconBrandTwitter size={18} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg" color="gray" variant="subtle">
+            <IconBrandYoutube size={18} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg" color="gray" variant="subtle">
+            <IconBrandInstagram size={18} stroke={1.5} />
+          </ActionIcon>
+        </Group>
+      </Container>
+    </footer>
+  );
+};
+
+export default FooterLinks;
