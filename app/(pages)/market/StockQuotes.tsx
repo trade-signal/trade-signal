ecd2562c18cc46973f2923d84d91a134b44cf83e
laihanbin <512412344@qq.com>
2025-01-08 11:49:35 +0800

    chore: market light dark style

--- app/(pages)/market/StockQuotes.tsx
+++ app/(pages)/market/StockQuotes.tsx
@@ -9,16 +9,18 @@ import {
   Text,
   Stack,
   Group,
-  Loader
+  Loader,
+  FloatingIndicator
 } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
 import { clientGet } from "@/shared/request";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { StockQuotesOrder } from "@/app/api/(stock)/stock-quotes/list/route";
 import SymbolChart from "@/app/components/charts/SymbolChart";
 import { StockQuotesRealTime } from "@prisma/client";
 import { formatNumber } from "@/app/components/tables/DataTable/util";
 import { formatPercent } from "@/app/components/tables/DataTable/util";
+import classes from "./StockQuotes.module.css";
 
 const transformSymbolChartData = (data: StockQuotesRealTime) => {
   return {
@@ -33,17 +35,24 @@ const transformSymbolChartData = (data: StockQuotesRealTime) => {
 
 const StockQuotes = () => {
   const [activeTab, setActiveTab] = useState<string | null>(null);
+  const [rootRef, setRootRef] = useState<HTMLDivElement | null>(null);
+  const [controlsRefs, setControlsRefs] = useState<Record<string, HTMLButtonElement | null>>({});
+  const setControlRef = (val: string) => (node: HTMLButtonElement) => {
+    controlsRefs[val] = node;
+    setControlsRefs(controlsRefs);
+  };
 
   const { data, isLoading } = useQuery({
     queryKey: ["stock-quotes"],
     queryFn: (): Promise<StockQuotesOrder[]> =>
-      clientGet("/api/stock-quotes/list", {}),
-    onSuccess(data) {
-      if (data) {
-        setActiveTab(data[0].code);
-      }
-    }
+      clientGet("/api/stock-quotes/list", {})
   });
+
+  useEffect(() => {
+    if (data) {
+      setActiveTab(data[0].code);
+    }
+  }, [data]);
 
   const symbolChartData = data?.map(item => ({
     code: item.code,
@@ -64,26 +73,21 @@ const StockQuotes = () => {
 
       <Tabs
         mt="lg"
-        variant="pills"
-        radius="lg"
+        variant="none"
+        radius="md"
         value={activeTab}
         onChange={value => setActiveTab(value)}
       >
-        <Tabs.List>
+        <Tabs.List ref={setRootRef} className={classes.list}>
           {data?.map(item => (
             <Tabs.Tab
-              color="gray.1"
-              style={{
-                justifyContent: "space-between",
-                color: "#333",
-                padding: "10px 20px",
-                marginRight: rem(60)
-              }}
               value={item.code}
               key={item.code}
+              ref={setControlRef(item.code)}
+              className={classes.tab}
             >
               <Stack gap={0} miw={rem(160)}>
-                <Text>{item.name}</Text>
+                <Text className={classes.tabName}>{item.name}</Text>
                 <Group m={0} gap="xs" justify="space-between">
                   <Text>{formatNumber(item.latest?.newPrice)}</Text>
                   <Text>{formatPercent(item.latest?.changeRate || 0)}</Text>
@@ -91,6 +95,12 @@ const StockQuotes = () => {
               </Stack>
             </Tabs.Tab>
           ))}
+
+          <FloatingIndicator
+            target={activeTab ? controlsRefs[activeTab] : null}
+            parent={rootRef}
+            className={classes.indicator}
+          />
         </Tabs.List>
 
         {symbolChartData?.map(item => (
