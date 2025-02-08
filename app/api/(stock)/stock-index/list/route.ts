5ee8efdcf240f9b6d470c9373122926f18818f85
heora <yueluo.yang@gmail.com>
2025-02-08 21:54:24 +0800

    fix: stock index quotes order error

--- app/api/(stock)/stock-index/list/route.ts
+++ app/api/(stock)/stock-index/list/route.ts
@@ -8,10 +8,36 @@ export type StockIndexList = {
   latest: StockIndexQuotes;
 };
 
+const getOrderList = (list: StockIndexQuotes[], limit: number) => {
+  const orderCodes = [
+    "000001", // 上证
+    "399001", // 深证
+    "399006", // 创业板
+    "399005", // 中小100
+    "000300", // 沪深300
+    "000016", // 上证50
+    "000688", // 科创50
+    "000852" // 中证1000
+  ];
+
+  const orderList = [] as StockIndexQuotes[];
+
+  while (orderCodes.length > 0) {
+    const code = orderCodes.shift();
+    const posIdx = list.findIndex(item => item.code === code);
+    if (posIdx !== -1) {
+      orderList.push(list[posIdx]);
+      list.splice(posIdx, 1);
+    }
+  }
+
+  orderList.push(...list);
+
+  return orderList.slice(0, limit);
+};
+
 export const GET = async (request: NextRequest) => {
   const searchParams = request.nextUrl.searchParams;
-  const orderBy = searchParams.get("orderBy") || "newPrice";
-  const order = searchParams.get("order") || "desc";
   const limit = Number(searchParams.get("limit")) || 5;
 
   const maxDate = await prisma.stockIndexQuotes.findFirst({
@@ -22,12 +48,12 @@ export const GET = async (request: NextRequest) => {
   const list = await prisma.stockIndexQuotes.findMany({
     where: {
       date: { equals: maxDate?.date }
-    },
-    take: limit,
-    orderBy: { [orderBy]: order }
+    }
   });
 
-  const transformData = list.map(stock => ({
+  const orderList = getOrderList(list, limit);
+
+  const transformData = orderList.map(stock => ({
     code: stock.code,
     name: stock.name,
     latest: stock
