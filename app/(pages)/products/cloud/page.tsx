adbd77ee00eb4648238634b39a2a51365a97265a
杨志强 <yangzhiqiang@winwin-inc.com>
2025-02-20 16:56:16 +0800

    feat: add stock cloud page

---
+++ app/(pages)/products/cloud/page.tsx
@@ -0,0 +1,12 @@
+import { Container } from "@mantine/core";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "大盘云图: 实时股票市场分析 - TradeSignal"
+};
+
+const CloudPage = () => {
+  return <Container fluid>大盘云图</Container>;
+};
+
+export default CloudPage;
