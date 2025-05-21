10fb0f0469e9972a9a1b6ec0b62650b186b0a2f1
杨志强 <yangzhiqiang@winwin-inc.com>
2025-05-21 16:47:06 +0800

    chore: init agents project

--- apps/core/src/app.module.ts
+++ apps/core/src/app.module.ts
@@ -1,15 +1,12 @@
 import { Module } from "@nestjs/common";
 import { ScheduleModule } from "@nestjs/schedule";
 
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-
 import { StockModule } from "./spider/stock/stock.module";
 import { NewsModule } from "./spider/news/news.module";
 
 @Module({
   imports: [ScheduleModule.forRoot(), StockModule, NewsModule],
-  controllers: [AppController],
-  providers: [AppService]
+  controllers: [],
+  providers: []
 })
 export class AppModule {}
